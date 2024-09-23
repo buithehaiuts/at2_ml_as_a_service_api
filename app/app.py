7a4d69856ee8550802b9387a8548e18696f49008
buithehai1994 <haibt0206@gmail.com>
2024-09-23 22:29:52 +1000

    Your commit message here

---
+++ app/app.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from starlette.responses import JSONResponse
+from joblib import load
+import pandas as pd
+
+# instantiate a FasAPI class
+app = FastAPI()
+
+# load model
+
+# create a GET endpoint for the real_root() function on root
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+

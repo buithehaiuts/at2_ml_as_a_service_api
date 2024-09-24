6eb2d3fc61485fa949c61682e791f23667b08a3c
buithehaiuts <127998752+buithehaiuts@users.noreply.github.com>
2024-09-24 12:07:20 +1000

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,25 +1,17 @@
 from fastapi import FastAPI
-from starlette.responses import JSONResponse
 
 # Instantiate a FastAPI class
 app = FastAPI()
 
-# Create a GET endpoint for the root
+# Create a GET endpoint for the root ("/") path
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+# Create a GET endpoint for the "/deploy" path
 @app.get("/deploy")
 async def home_root():
     return {"message": "Render deployment"}
 
-# Create a POST endpoint for making mock predictions
-@app.post("/predict")
-async def predict(data: dict):
-    # Mock prediction logic
-    mock_prediction = {"prediction": "mock_prediction_result"}
-    
-    return JSONResponse(content=mock_prediction)
-
-# To run the FastAPI app, you would use:
-# uvicorn api:app --reload
+# To run the FastAPI app, use the following command:
+# uvicorn your_file_name:app --reload

83609c53fe5e6c95cbebc5d3027886cd2be2cb82
buithehaiuts <127998752+buithehaiuts@users.noreply.github.com>
2024-09-26 13:33:18 +1000

    Update api.py

--- app/backend/api.py
+++ app/backend/api.py
@@ -1,1 +1,21 @@
+from fastapi import FastAPI
 
+# Instantiate a FastAPI class
+app = FastAPI()
+
+# Create a GET endpoint for the root ("/") path
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+# Create a GET endpoint for deployment testing
+@app.get("/deploy")
+async def home_root():
+    return {"message": "Render deployment"}
+
+# Create a POST endpoint for mock prediction
+@app.post("/predict")
+async def predict(data: dict):
+    # Here you would normally load a model and predict based on 'data'
+    mock_prediction = {"prediction": "This is a mock prediction."}
+    return mock_prediction

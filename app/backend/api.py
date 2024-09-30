684b585a78b6b164f580ad3fe9e03184232eed76
buithehaiuts <127998752+buithehaiuts@users.noreply.github.com>
2024-09-30 17:03:28 +1000

    Update api.py

--- app/backend/api.py
+++ app/backend/api.py
@@ -1,17 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
 from pydantic import BaseModel
 from typing import List
 import joblib  # or you can use pickle
 import pandas as pd  # Assuming you'll use pandas for data handling
 import os  # For constructing file paths
 
-# Pydantic model for a Sale
+class HealthCheck(BaseModel):
+    """Response model to validate and return when performing a health check."""
+    status: str
+
 class Sale(BaseModel):
     id: int
     amount: float
     date: str
 
-# Pydantic model for the response containing multiple sales
 class SalesResponse(BaseModel):
     sales: List[Sale]
 
@@ -36,13 +38,22 @@ class SalesAPI:
 
     def setup_routes(self):
         # Health check endpoint
-        @self.app.get("/health/")
-        def health_check():
-            return {"status": "healthy"}
+        @self.app.get(
+            "/health/",
+            tags=["healthcheck"],
+            response_model=HealthCheck,
+            summary="Perform a Health Check",
+            response_description="Return HTTP Status Code 200 (OK)",
+            status_code=status.HTTP_200_OK,
+        )
+        async def health_check():
+            """Health Check endpoint."""
+            return HealthCheck(status="OK")
 
         # Endpoint for national sales forecast
         @self.app.get("/sales/national/")
         async def national_sales_forecast(date: str, item_id: str, store_id: str):
+            """Get national sales forecast."""
             # Prepare input for prediction
             format_data = {
                 'date': [date],
@@ -56,6 +67,7 @@ class SalesAPI:
         # Endpoint for predicting sales based on store and item
         @self.app.post("/sales/stores/items/")
         async def predict_sales(date: str, item_id: str, store_id: str):
+            """Predict sales based on store and item."""
             # Implement your logic for predicting sales here
             predictive_input = {
                 'date': [date],

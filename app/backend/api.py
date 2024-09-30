5e6f4c789034a67b89a81e71701ff64683d50380
buithehaiuts <127998752+buithehaiuts@users.noreply.github.com>
2024-09-30 19:13:54 +1000

    Update api.py

--- app/backend/api.py
+++ app/backend/api.py
@@ -107,4 +107,4 @@ async def predict_sales(date: str, item_id: str, store_id: str):
 if __name__ == "__main__":
     import uvicorn
     port = int(os.getenv("PORT", 8000))  # Get the port from environment variable or default to 8000
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=port)

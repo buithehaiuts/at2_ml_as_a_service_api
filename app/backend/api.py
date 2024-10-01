1488e49e7f87309d2db02f9e9f003fd8c2d5b435
buithehaiuts <127998752+buithehaiuts@users.noreply.github.com>
2024-10-01 13:48:32 +1000

    Update api.py

--- app/backend/api.py
+++ app/backend/api.py
@@ -66,10 +66,10 @@ async def startup_event():
     
     # List of model filenames
     model_files = {
-        'prophet': dataset_path/'prophet.pkl',
-        'prophet_event': dataset_path/'prophet_event.pkl',
-        'prophet_holiday': dataset_path/'prophet_holiday.pkl',
-        'prophet_month': dataset_path/'prophet_month.pkl'
+        'prophet': dataset_path / 'prophet.pkl',
+        'prophet_event': dataset_path / 'prophet_event.pkl',
+        'prophet_holiday': dataset_path / 'prophet_holiday.pkl',
+        'prophet_month': dataset_path / 'prophet_month.pkl'
     }
 
     for model_path, filename in model_files.items():

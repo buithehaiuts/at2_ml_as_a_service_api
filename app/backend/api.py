7e59835dbccf81a66bfc642c007a45bd8428193e
buithehaiuts <127998752+buithehaiuts@users.noreply.github.com>
2024-10-01 22:18:07 +1000

    Update api.py

--- app/backend/api.py
+++ app/backend/api.py
@@ -68,10 +68,10 @@ def validate_date(date_str: str) -> bool:
 async def startup_event():
     # Define model file paths using Path
     model_files = {
-        'prophet': './model/prophet.pkl',
-        'prophet_event': './model/prophet_event.pkl',
-        'prophet_holiday': './model/prophet_holiday.pkl',
-        'prophet_month': './model/prophet_month.pkl'
+        'prophet': './models/prophet.pkl',
+        'prophet_event': './models/prophet_event.pkl',
+        'prophet_holiday': './models/prophet_holiday.pkl',
+        'prophet_month': './models/prophet_month.pkl'
     }
     for model_name, model_path in model_files.items():
         logger.info(f"Attempting to load model from: {model_path}")  # Log the model loading path

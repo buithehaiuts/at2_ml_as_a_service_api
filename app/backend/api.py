9de25a6bb5b8a012f5360c4f89449eaf4b0e0379
buithehaiuts <127998752+buithehaiuts@users.noreply.github.com>
2024-10-04 19:41:05 +1000

    Update api.py

--- app/backend/api.py
+++ app/backend/api.py
@@ -100,11 +100,11 @@ async def startup_event():
 
     # Load encoders
     encoder_files = {
-        'item_id': 'backend/item_id_encoder.pkl',
-        'store_id': 'backend/store_id_encoder.pkl',
-        'state_id': 'backend/state_id_encoder.pkl',
-        'cat_id': 'backend/cat_id_encoder.pkl',
-        'dept_id': 'backend/dept_id_encoder.pkl'
+        'item_id': 'app/backend/item_id_encoder.pkl',
+        'store_id': 'app/backend/store_id_encoder.pkl',
+        'state_id': 'app/backend/state_id_encoder.pkl',
+        'cat_id': 'app/backend/cat_id_encoder.pkl',
+        'dept_id': 'app/backend/dept_id_encoder.pkl'
     }
 
     for encoder_name, encoder_path in encoder_files.items():
